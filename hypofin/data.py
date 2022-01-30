678dde6c86d1386b890420f421a01083783edeec
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-01-30 20:47:51 +0100

    feature: predict economic variables

---
+++ hypofin/data.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+from cachetools import cached, TTLCache
+import pandas as pd
+import re
+import requests
+import yfinance as yf
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=24 * 60 * 60))
+def global_cape_ratio() -> float:
+    return pd.read_html("https://siblisresearch.com/data/world-cape-ratio/")[0][
+        "Global Stock Markets CAPE Ratio"
+    ][0]
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=24 * 60 * 60))
+def euro_inflation() -> float:
+    text_percent = pd.read_html(
+        "https://www.rateinflation.com/inflation-rate/euro-area-historical-inflation-rate/"
+    )[0]["Annual"].dropna()[0]
+    assert text_percent[-1] == "%"
+    return float(text_percent[:-1]) / 100
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=24 * 60 * 60))
+def bond_yield() -> float:
+    html = requests.get(
+        "http://www.worldgovernmentbonds.com/country/united-states/"
+    ).text
+    text = BeautifulSoup(html).text
+    text_percent = re.findall(
+        "The United States 10Y Government Bond has a (\d+\.\d+)% yield.", text
+    )[0]
+    return float(text_percent) / 100
+
+
+@cached(cache=TTLCache(maxsize=1, ttl=24 * 60 * 60))
+def stock_prices() -> pd.Series:
+    return yf.Ticker("^GSPC").history(period="max", interval="1m")["Close"]
---
+++ hypofin/expectations.py
@@ -0,0 +1,13 @@
+import hypofin.data as data
+
+
+def inflation() -> float:
+    return data.euro_inflation()
+
+
+def bond_returns() -> float:
+    return data.bond_yield()
+
+
+def stock_returns() -> float:
+    return inflation() + 1 / data.global_cape_ratio()
