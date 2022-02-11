1b5d4509fb25b9115ac99650de152982fe697f6d
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-02-11 23:19:39 +0100

    feature: add tax systems

---
+++ hypofin/tax_system.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from typing import Union
+
+
+accepted_types = Union[np.ndarray, pd.Series]
+
+
+@dataclass(frozen=True)
+class TaxSystem:
+    def tax_savings(self, monthly_savings: accepted_types) -> accepted_types:
+        raise NotImplementedError()
+
+
+@dataclass(frozen=True)
+class CapitalGainsTaxSystem(TaxSystem):
+    tax: float
+
+    def tax_savings(self, monthly_savings: accepted_types) -> accepted_types:
+        first_savings = np.array(monthly_savings)[0]
+        excess_savings = monthly_savings - first_savings
+        negative_excess = np.minimum(excess_savings, 0)
+        positive_excess = np.maximum(excess_savings, 0)
+        return monthly_savings + negative_excess + positive_excess * (1 - self.tax)
+
+
+@dataclass(frozen=True)
+class WealthTaxSystem(TaxSystem):
+    tax: float
+
+    def tax_savings(self, monthly_savings: accepted_types) -> accepted_types:
+        monthly_multiplier = (1 - self.tax) ** (1 / 12)
+        return monthly_savings * np.cumprod(
+            np.full_like(monthly_savings, monthly_multiplier)
+        )
+
+
+tax_systems = {
+    "poland": CapitalGainsTaxSystem(tax=0.19),
+    "netherlands": WealthTaxSystem(tax=0.012),
+}
