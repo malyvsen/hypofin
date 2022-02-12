bd67133a85e46a018d14568c1c488f9f97e77a46
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-02-12 11:00:55 +0100

    feature: sample savings evolution

--- hypofin/simulator.py
+++ hypofin/simulator.py
@@ -20,5 +20,19 @@ class Simulator:
             )
         )
 
-    def sample_returns(self, num_months: int):
-        return np.exp(self.log_return_distribution.rvs(num_months)) - 1
+    def sample_returns(self, num_steps: int) -> np.ndarray:
+        return np.exp(self.log_return_distribution.rvs(num_steps)) - 1
+
+    def sample_savings(
+        self, num_steps: int, start_amount: float, added_per_step: float
+    ) -> np.ndarray:
+        growth_series = 1 + np.concatenate(
+            [[0], self.sample_returns(num_steps - 1)], axis=0
+        )
+        cumulative_growth = np.cumprod(growth_series)
+        start_amount_growth = start_amount * cumulative_growth
+        added = np.concatenate(
+            [[0], np.full(shape=num_steps - 1, fill_value=added_per_step)], axis=0
+        )
+        added_growth = np.cumsum(added / cumulative_growth) * cumulative_growth
+        return start_amount_growth + added_growth
