02ac70f60799f3b52c1cebe9cb603d6a250fabc3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-02-14 22:38:21 +0100

    feature: predict bank portfolio

--- chalicelib/predictions.py
+++ chalicelib/predictions.py
@@ -2,15 +2,28 @@ import chalicelib.data as data
 from .portfolio import RisklessPortfolio, RiskyPortfolio
 
 
+def bond_portfolio():
+    return RisklessPortfolio(
+        return_per_step=(1 + annual_to_monthly(data.bond_yield()))
+        / (1 + monthly_inflation())
+        - 1
+    )
+
+
+def stock_portfolio():
+    return RiskyPortfolio.from_historical_prices(
+        historical_prices=data.stock_prices(),
+        expected_return=1 / data.global_cape_ratio(),
+    )
+
+
+def bank_portfolio():
+    return RisklessPortfolio(return_per_step=1 / (1 + monthly_inflation()) - 1)
+
+
+def monthly_inflation():
+    annual_to_monthly(data.euro_inflation())
+
+
 def annual_to_monthly(annual_return: float) -> float:
     return (1 + annual_return) ** (1 / 12) - 1
-
-
-bond_portfolio = RisklessPortfolio(
-    return_per_step=annual_to_monthly(
-        (1 + data.bond_yield()) / (1 + data.euro_inflation()) - 1
-    )
-)
-stock_portfolio = RiskyPortfolio.from_historical_prices(
-    historical_prices=data.stock_prices(), expected_return=1 / data.global_cape_ratio()
-)
