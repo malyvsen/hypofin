2dc7f2cd362b8fc4bf0ad13b2cf52de9eeba85b8
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-02-27 16:26:05 +0100

    chore: unify bash script arguments

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 poetry export -f requirements.txt --output requirements.txt --without-hashes
 sed -i "/numpy/d" requirements.txt
 sed -i "/scipy/d" requirements.txt
-poetry run chalice deploy --profile $profile_name
+poetry run chalice deploy --profile $profile
 rm requirements.txt
