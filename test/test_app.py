8f78c6d2fa8b08a25505f977fbd5db48e3401a48
malyvsen <5940672+malyvsen@users.noreply.github.com>
2022-04-16 15:21:50 +0200

    test: add app test for poland

--- test/test_app.py
+++ test/test_app.py
@@ -2,7 +2,24 @@ from app import response
 from chalicelib import User, countries
 
 
-def test_response_fields():
+def test_netherlands():
+    user = User(
+        risk_preference=36,
+        monthly_savings=5,
+        goal_price=230,
+        current_savings=180,
+        country=countries["netherlands"],
+    )
+    test_me = response(user)
+    assert len(test_me["aggregate_trajectories"]) == 4
+    assert set(test_me.keys()) == {
+        "aggregate_trajectories",
+        "example_trajectories",
+        "allocation",
+    }
+
+
+def test_poland():
     user = User(
         risk_preference=36,
         monthly_savings=5,
