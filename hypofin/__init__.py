d7b58302d4f85a4462d682dcfe17078c6095a1bb
malyvsen <5940672+malyvsen@users.noreply.github.com>
2023-08-08 09:02:59 +0200

    chore: remove obsolete files

--- hypofin/__init__.py
+++
@@ -1,7 +0,0 @@
-import hypofin.predictions as predictions
-
-from .country import Country, countries
-from .return_source import ReturnSource
-from .tax_system import TaxSystem
-from .trajectory import AggregateTrajectory, ExplainedTrajectory, Trajectory
-from .user import User
--- hypofin/return_utils.py
+++
@@ -1,2 +0,0 @@
-def annual_to_monthly(annual_return: float) -> float:
-    return (1 + annual_return) ** (1 / 12) - 1
